f4d19ae2aaea61f6b13b2bda2ecb242f29902511
erlingOya <Trekanten54321>
2022-05-07 17:11:26 +0200

    Added class for creating projected 2D plot and add plot to image

--- utils.py
+++ utils.py
@@ -30,3 +30,35 @@ class ViewGui:
     def wait_key(self, time_ms):
         """Runs the highgui event loop and receives keypress events"""
         return cv2.waitKey(time_ms)
+
+class Projection2DPlot:
+    """Plots projected point on to a background image"""
+    def __init__(self, background, actual_width, actual_height):
+        self.background = background
+        self.image_height, self.image_width = self.background.shape[0:2]
+        """Actual size of projection surface given in meters"""
+        self.width = actual_width
+        self.height = actual_height
+
+    def plot_point(self, x, y):
+        x_pixel = round(x*(self.image_height/self.height))
+        y_pixel = round(y*(self.image_width/self.width))
+        return cv2.circle(self.background, (x_pixel, y_pixel), 20, (0, 0, 255), -1)
+
+    @staticmethod
+    def add_to_frame(frame, plot, relative_size):
+        frame_width = frame.shape[1]
+        frame_height = frame.shape[0]
+        plot_width = int(frame_width * relative_size)
+        plot_height = int(frame_height * relative_size)
+        dim = (plot_width, plot_height)
+
+        # resize image
+        resized = cv2.resize(plot, dim, interpolation=cv2.INTER_AREA)
+
+        new_frame = frame.copy()
+        print(frame.shape)
+        print(resized.shape)
+        new_frame[:plot_height, (frame_width-plot_width):frame_width] = resized
+
+        return new_frame
