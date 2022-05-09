916d32689f472f70139b9953187993495b581fe2
johanbir <johanbir@ifi.uio.no>
2022-05-09 16:01:37 +0200

    added naive tracking of center points

--- MotionDetection.py
+++ MotionDetection.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2
 import utils
 
+
+
 def run_motion_detction():
-    # Connect to the camera.
-    # Change to video file if you want to use that instead.
-    video_source = 2
+    video_source = 0
     cap = cv2.VideoCapture(video_source)
     if not cap.isOpened():
         print(f"Could not open video source {video_source}")
@@ -14,8 +14,10 @@ def run_motion_detction():
         print(f"Successfully opened video source {video_source}")
 
     mog = cv2.createBackgroundSubtractorMOG2()
+    centers_history = np.empty(20, dtype=list)
     with utils.ViewGui() as gui:
         while True:
+            
             success, frame = cap.read()
 
             if not success:
@@ -24,7 +26,7 @@ def run_motion_detction():
             fgmask = mog.apply(frame, 123, 0.01)
             
             blured = cv2.GaussianBlur(src=fgmask, ksize=(9, 9), sigmaX=0)
-            _, thresh = cv2.threshold(blured, 200, 255, cv2.THRESH_BINARY)
+            _, thresh = cv2.threshold(blured, 220, 255, cv2.THRESH_BINARY)
 
             # 4. Dilute the image a bit to make differences more seeable; more suitable for contour detection
             kernel = np.ones((5, 5))
@@ -33,18 +35,39 @@ def run_motion_detction():
             contours, _ = cv2.findContours(image=dilated, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
             #cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
             #                 lineType=cv2.LINE_AA)
+            centers = []
             for contour in contours:
-                if cv2.contourArea(contour) < 150:
-                    #too small!
+                if cv2.contourArea(contour) < 400:
+                #too small!
                     continue
-                
+
                 (x, y, w, h) = cv2.boundingRect(contour)
                 cv2.rectangle(img=frame, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
-                
-            gui.show_frame(frame)
+                center = (x+w//2, y+h//2)
+                centers.append(center)
+
+            centers_history[0] = centers
+
+            col = cv2.cvtColor(dilated, cv2.COLOR_GRAY2BGR)
+            col[:,:,2] = 0
+            col[:,:,1] = 0
+
+            for centers in centers_history:
+                if centers is None:
+                    continue
+                for center in centers:
+                    cv2.circle(frame, center, 1, (0,0,255), 2)
+
+            centers_history = np.roll(centers_history, 1)
+
+            ttt = cv2.addWeighted(frame, 1, col, 0.40, 0.0)
+
+            gui.show_frame(ttt)
             key = gui.wait_key(1)
             if key == ord("q"):
-                break
+                break            
+            if key == ord(" "):
+                centers_history = np.empty(20, dtype=list)
 
 
 def main():
