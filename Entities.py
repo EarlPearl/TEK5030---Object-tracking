feaecf3cd411b8badcf48b65c8136b6b92fca0a4
johanbir <johanbir@ifi.uio.no>
2022-05-13 09:54:58 +0200

    Created class to track and id objects

---
+++ Entities.py
@@ -0,0 +1,68 @@
+class Entity:
+
+    def __init__(self, pos, epoch, color=(0,0,255)):
+        self.pos = [pos]
+        self.last_updated = epoch
+        self.queued_point = None
+        self.color = color
+
+    def queue(self, point):
+        if self.queued_point is None:
+            self.queued_point = point
+            return True
+        return False
+
+    def update(self, epoch):
+        if not self.queued_point is None:
+            self.pos.append(self.queued_point)
+            self.queued_point = None
+            self.last_updated = epoch
+        return self.last_updated
+    
+    def draw(self, frame):
+        for point in self.pos:
+            cv2.circle(frame, point, 1, self.color, 2)
+
+    @property
+    def x(self):
+        return self.pos[-1][0]
+    @property
+    def y(self):
+        return self.pos[-1][1]
+        
+
+class Entities:
+    def __init__(self, MAX_ENTS=100, threshold = 5, decay=100):
+        self.entities = []
+        self.queue = []
+        self.epoch = 0
+        self.MAX_ENTS = MAX_ENTS
+        self.threshold = threshold
+        self.decay = decay
+    
+    def queue_point(self, point):
+        #TODO maybe make it so that points are optimised not just given to the first entity within bounds?
+        x, y = point
+        for ent in self.entities:
+            if (x < ent.x + self.threshold and \
+                x > ent.x - self.threshold and \
+                y < ent.y + self.threshold and \
+                y > ent.y - self.threshold):
+                if ent.queue(point):
+                    return
+        self.queue.append(point)
+    
+    def update(self):
+        for ent in self.entities:
+            if ent.update(self.epoch) <= self.epoch - self.decay:
+                self.entities.remove(ent)
+
+        while len(self.queue) > 0 and len(self.entities) < self.MAX_ENTS:
+            point = self.queue.pop()
+            self.entities.append(Entity(point, self.epoch))
+
+        self.epoch += 1
+
+    def draw(self, frame):
+        for ent in self.entities:
+            ent.draw(frame)
