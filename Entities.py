b3fb636426aea04c4302360eaebec4fc62c51081
johanbir <johanbir@ifi.uio.no>
2022-05-18 18:46:43 +0200

    masse edits

--- Entities.py
+++ Entities.py
@@ -1,74 +1,148 @@
 import cv2
 import numpy as np
-class Entity:
+from COLORS import COLORS
 
-    def __init__(self, pos, epoch, color=(0,0,255)):
-        self.pos = [pos]
+class Entity:
+    def __init__(self, pos, offset, epoch, MAX_POINTS, color=(0,0,255)):
+        self.age = 0 #number of times updated
+        self.MAX_POINTS = MAX_POINTS
+        self.offset = offset
+        self._pos = []
+        self.pos = pos
         self.last_updated = epoch
         self.queued_point = None
         self.color = color
         self.id = (str(np.random.choice([chr(i) for i in range(ord("A"), ord("Z"))], 2)).strip("[]").replace("'","") \
             + "-" + str(np.random.choice([i for i in range(10)], 5)).strip("[]").replace("'","")).replace(" ", "")
 
-    def queue(self, point):
+    def queue(self, point, offset):
         if self.queued_point is None:
-            self.queued_point = point
+            self.queued_point = (point, offset)
             return True
         return False
 
     def update(self, epoch):
         #TODO, check feature matches.
         #if none, check difference with last seen area
+
+        #TODO add linreg of last N positions to predict next
+
+
+        #TODO use predicted next point to look for features/match
+
         if not self.queued_point is None:
-            self.pos.append(self.queued_point)
+            self.pos, self.offset = self.queued_point
             self.queued_point = None
             self.last_updated = epoch
-        return self.last_updated
+            self.age += 1
+
+        score = self.age 
+        return score
     
     def draw(self, frame):
+
+        x, y = self.predicted_pos
+        w, h = self.offset
+        #draw bounding box
+
+        cv2.rectangle(img=frame, pt1=(x-w, y-h), pt2=(x + w, y + h), color=(0, 255, 0), thickness=1)
+
+        #draw path
         for point in self.pos:
             cv2.circle(frame, point, 1, self.color, 2)
+        #draw current position
+        cv2.circle(frame, (self.x, self.y), 1, self.color, 10)
+
+        #draw lable
+        text = f"{self.id}: {self.age}"
+        font = cv2.FONT_HERSHEY_PLAIN
+        font_scale = 1
+        thickness = 1
+        x = self.x - self.offset[0]
+        y = self.y - self.offset[1]
+        
+        (w, h), _ = cv2.getTextSize(text, font, font_scale, thickness)
+        cv2.rectangle(frame, (x, y - 12), (x + w, y), self.color, -1)
+        cv2.rectangle(frame, (x, y - 12), (x + w, y), (0,255,0), 1)
+        cv2.putText(frame, text, (x, y), font, font_scale, (0,0,0), thickness)
 
     @property
+    def pos(self):
+        return self._pos
+    @pos.setter
+    def pos(self, new_pos):
+        self._pos.append(new_pos)
+        if len(self._pos) > self.MAX_POINTS:
+            self._pos.pop(0)
+    @property
+    def predicted_pos(self):
+        return self._pos[-1]
+    @property
     def x(self):
-        return self.pos[-1][0]
+        return self._pos[-1][0]
     @property
     def y(self):
-        return self.pos[-1][1]
+        return self._pos[-1][1]
         
 
 class Entities:
-    def __init__(self, MAX_ENTS=100, threshold = 5, decay=100):
+    def __init__(self, MAX_ENTS=10, threshold=50, decay=30, MAX_POINTS = 1000):
         self.entities = []
         self.queue = []
         self.epoch = 0
         self.MAX_ENTS = MAX_ENTS
-        self.threshold = threshold
+        self.MAX_POINTS = MAX_POINTS
+        self.threshold = threshold**2
         self.decay = decay
-    
-    def queue_point(self, point):
+        self.colors = COLORS()
+
+    def flush(self):
+        self.entities = []
+        self.queue = []
+
+    def queue_point(self, point, offset):
         #TODO maybe make it so that points are optimised not just given to the first entity within bounds?
-        x, y = point
+        x1, y1 = point
+        dists = {}
         for ent in self.entities:
-            if (x < ent.x + self.threshold and \
-                x > ent.x - self.threshold and \
-                y < ent.y + self.threshold and \
-                y > ent.y - self.threshold):
-                if ent.queue(point):
-                    return
-        self.queue.append(point)
-    
+            x2, y2 = ent.predicted_pos
+            d = (x1 - x2)**2 + (y1 - y2)**2
+            if d <= self.threshold:
+                dists[d] = ent
+            """if (x1 < x2 + self.threshold and \
+                x1 > x2 - self.threshold and \
+                y1 < y2 + self.threshold and \
+                y1 > y2 - self.threshold):
+                if ent.queue(point, offset):
+                    return"""
+        if len(dists.keys()) > 0:
+            dists[min(dists.keys())].queue(point, offset)
+        else:
+            self.queue.append((point, offset))
+        return
+
     def update(self):
+        self.best_score = np.zeros(5)
+        self.best = [None for i in range(5)]
         for ent in self.entities:
-            if ent.update(self.epoch) <= self.epoch - self.decay:
+            score = ent.update(self.epoch)
+            if self.epoch - ent.last_updated > self.decay:
                 self.entities.remove(ent)
+                continue
+
+            worst = np.argmin(self.best_score)
+            if score > self.best_score[worst]:
+                self.best_score[worst] = score
+                self.best[worst] = ent
 
         while len(self.queue) > 0 and len(self.entities) < self.MAX_ENTS:
-            point = self.queue.pop()
-            self.entities.append(Entity(point, self.epoch))
+            point, offset = self.queue.pop()
+            self.entities.append(Entity(point, offset, self.epoch, self.MAX_POINTS, self.colors.next))
 
         self.epoch += 1
 
     def draw(self, frame):
-        for ent in self.entities:
+        for ent in self.best:
+            if ent is None:
+                continue
             ent.draw(frame)
--- MotionDetection.py
+++ MotionDetection.py
@@ -5,7 +5,8 @@ from Entities import Entities
 
 
 def run_motion_detction():
-    video_source = 2
+    video_source = 0
+    mode = 1
     cap = cv2.VideoCapture(video_source)
     if not cap.isOpened():
         print(f"Could not open video source {video_source}")
@@ -25,17 +26,13 @@ def run_motion_detction():
 
             fgmask = mog.apply(frame, 123, 0.01)
             
+
             blured = cv2.GaussianBlur(src=fgmask, ksize=(9, 9), sigmaX=0)
             _, thresh = cv2.threshold(blured, 220, 255, cv2.THRESH_BINARY)
-
-            # 4. Dilute the image a bit to make differences more seeable; more suitable for contour detection
-            kernel = np.ones((5, 5))
+            kernel = np.ones((9, 9))
             dilated = cv2.dilate(thresh, kernel, 1)
-            
-            contours, _ = cv2.findContours(image=dilated, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-            #cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2,
-            #                 lineType=cv2.LINE_AA)
 
+            contours, _ = cv2.findContours(image=dilated, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
 
             for contour in contours:
                 if cv2.contourArea(contour) < 400:
@@ -43,46 +40,42 @@ def run_motion_detction():
                     continue
 
                 (x, y, w, h) = cv2.boundingRect(contour)
-                cv2.rectangle(img=frame, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
                 center = (x+w//2, y+h//2)
-                entities.queue_point(center)
-
-            col = cv2.cvtColor(dilated, cv2.COLOR_GRAY2BGR)
-            col[:,:,2] = 0
-            col[:,:,1] = 0
-
-            ttt = cv2.addWeighted(frame, 1, col, 0.40, 0.0)
+                entities.queue_point(center, (w//2, h//2))
 
             entities.update()
-            entities.draw(ttt)
+            if mode == 1:
+                entities.draw(frame)
+                out = frame
+                
+            if mode == 2:
+                out = fgmask
+                
+            if mode == 3:
+                out = thresh
+                
+            if mode == 4:
+                out = frame
+                cv2.drawContours(frame, contours, -1, (255,0,0), 1)
+                            
+            gui.show_frame(out)
+            #cv2.drawContours(image=frame, contours=contours, contourIdx=-1, color=(0, 0, 255), thickness=1, lineType=cv2.LINE_AA)
 
-            gui.show_frame(ttt)
+            
             key = gui.wait_key(1)
+            if key == ord("1"):
+                mode = 1
+            if key == ord("2"):
+                mode = 2
+            if key == ord("3"):
+                mode = 3
+            if key == ord("4"):
+                mode = 4    
+
             if key == ord("q"):
                 break            
             if key == ord(" "):
-                pass
-
-
-def main():
-    # Connect to the camera.
-    # Change to video file if you want to use that instead.
-    video_source = 0
-    cap = cv2.VideoCapture(video_source)
-    if not cap.isOpened():
-        print(f"Could not open video source {video_source}")
-        return
-    else:
-        print(f"Successfully opened video source {video_source}")
-
-    # Read the first frame.
-    success, frame = cap.read()
-    if not success:
-        return
-
-    # Stop video source.
-    # cap.release()
-
+                entities.flush()
 
 if __name__== '__main__':
     run_motion_detction()
